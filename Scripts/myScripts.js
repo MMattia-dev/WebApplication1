243503b3ae69ca7c48a9a1409df54b24b73e26a4
MMattia-dev <kromolski@gmail.com>
2022-03-02 17:53:25 +0100

    Dodaj pliki projektów.

---
+++ Scripts/myScripts.js
@@ -0,0 +1,23 @@
+﻿window.onload = function () {
+    
+};
+
+$(document).ready(function () {
+    
+});
+
+
+function show_sidenav() {
+    $('.sidenav').animate({ 'width': 'toggle' }, 400);
+    $('.sidenav-icon').toggleClass("change-icon");
+    
+
+};
+
+function toggle_navbar_toggle(){
+    $('.navbar-toggle').toggleClass("color");
+};
+
+
+
+
